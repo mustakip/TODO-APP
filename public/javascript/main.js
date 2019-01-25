0aff00e7ad1e545e93a4628839089e2d425cf130
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-01-25 15:35:23 +0530

    Add functionality to create TODO and adding task
    For now no tasks are stored.

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,22 +1,51 @@
+const getTodoDiv = document => document.getElementById('todo_list');
+const getTask = document => document.getElementById('task').value;
+const createElement = element => document.createElement(element);
+
 const fetchTitleAndDescription = function() {
   const title = document.getElementById('title').value;
   const description = document.getElementById('description').value;
+  createTodo(title, description);
   fetch('/createTodo', {
     method: 'POST',
     body: JSON.stringify({ title, description })
-  })
-    .then(res => res.text())
-    .then(details => {
-      generateTodoDiv(title, description);
-    });
+  });
 };
 
-const generateTodoDiv = function(title, description) {
-  const todoDiv = document.getElementById('todo_list');
-  const newTodo = document.createElement('div');
-  newTodo.className = 'todo_item';
-  newTodo.innerHTML = `<h1> ${title}</h1><h2> ${description}</h2>`;
-  todoDiv.appendChild(newTodo);
+const addTask = function() {
+  const task = getTask(document);
+  const list = document.getElementById('tasks');
+  list.innerHTML += `<li> ${task}</li>`;
+};
+
+const createTodoTask = function() {
+  const tasks = createElement('ul');
+  tasks.id = 'tasks';
+  return tasks;
+};
+
+const decorateTodo = function(title, description) {
+  return `<h2> ${title}</h2>
+          <h3> ${description}</h3>
+          <input type= "text" id="task" />
+          <button id="add_task">Add task</button>`;
+};
+
+const createTodoAttributes = function(title, description) {
+  const todo = createElement('div');
+  todo.className = 'todo_item';
+  todo.id = 'todo_item';
+  todo.innerHTML = decorateTodo(title, description);
+  todo.appendChild(createTodoTask());
+  return todo;
+};
+
+const createTodo = function(title, description) {
+  const todoDiv = getTodoDiv(document);
+  const todo = createTodoAttributes(title, description);
+
+  todoDiv.appendChild(todo);
+  document.getElementById('add_task').onclick = addTask;
 };
 
 window.onload = () => {
--- src/todoHandlers.js
+++ src/todoHandlers.js
@@ -1,10 +1,16 @@
 const Todo = require('../src/model/todo');
+const fs = require('fs');
 const { send } = require('./handlers');
+const todos = fs.readFileSync('./private/todo.json', 'utf8');
 
 const createTodo = function(req, res) {
   const { title, description } = JSON.parse(req.body);
   const todo = new Todo(title, description);
-  send(res, JSON.stringify(todo));
+  const parsedTodos = JSON.parse(todos);
+  parsedTodos.push(todo);
+  fs.writeFile('./private/todo.json', JSON.stringify(parsedTodos), () => {
+    send(res, JSON.stringify(todo));
+  });
 };
 
 module.exports = {
