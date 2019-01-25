ecd027b834c531aeeb81c1e05c9d88e6381a85d0
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-01-25 14:30:37 +0530

    Create dummy title and description div using fetch

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,12 +1,22 @@
 const fetchTitleAndDescription = function() {
-  let title = document.getElementById('title').value;
-  let description = document.getElementById('description').value;
+  const title = document.getElementById('title').value;
+  const description = document.getElementById('description').value;
   fetch('/createTodo', {
     method: 'POST',
     body: JSON.stringify({ title, description })
   })
     .then(res => res.text())
-    .then(details => alert(details));
+    .then(details => {
+      generateTodoDiv(title, description);
+    });
+};
+
+const generateTodoDiv = function(title, description) {
+  const todoDiv = document.getElementById('todo_list');
+  const newTodo = document.createElement('div');
+  newTodo.className = 'todo_item';
+  newTodo.innerHTML = `<h1> ${title}</h1><h2> ${description}</h2>`;
+  todoDiv.appendChild(newTodo);
 };
 
 window.onload = () => {
