caba290a99f3af5d0f6060aa607b563a83452f0c
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-01-29 10:38:59 +0530

    Extracted functions to get divs

--- public/javascript/userTodo.js
+++ public/javascript/userTodo.js
@@ -1,13 +1,24 @@
+const getTitleDiv = document => document.getElementById('todo_title');
+const getDescriptionDiv = document =>
+  document.getElementById('todo_description');
+const getTasksDiv = document => document.getElementById('todo_tasks');
+
+const getTodoTitle = document =>
+  document.getElementById('todo_title').textContent;
+
+const getTodoDescription = document =>
+  document.getElementById('todo_description').textContent;
+
+const getTask = document => document.getElementById('task_input_box').value;
+const getEditTitleDiv = document => document.getElementById('edit_title');
+const getEditDescriptionDiv = document =>
+  document.getElementById('edit_description');
+
 const generateTaskView = function(tasks) {
   const taskIDs = Object.keys(tasks);
   const taskHTML = taskIDs.map(id => `<li>${tasks[id].task}</li>`);
   return `<ul>${taskHTML.join('')}</ul>`;
 };
-
-const getTitleDiv = document => document.getElementById('todo_title');
-const getDescriptionDiv = document =>
-  document.getElementById('todo_description');
-const getTasksDiv = document => document.getElementById('todo_tasks');
 
 const displayTodo = function(todo) {
   const titleDiv = getTitleDiv(document);
@@ -22,13 +33,13 @@ const displayTodo = function(todo) {
 const updateTodoPage = function(todo) {
   const editTitleBtn = `<button id="edit_title_btn" onclick="createEditTitleBox()">Edit</button>`;
   const editDescriptionBtn = `<button id="edit_description_btn" onclick="createEditDescriptionBox()">Edit</button>`;
-  document.getElementById('edit_title').innerHTML = editTitleBtn;
-  document.getElementById('edit_description').innerHTML = editDescriptionBtn;
+  getEditTitleDiv(document).innerHTML = editTitleBtn;
+  getEditDescriptionDiv(document).innerHTML = editDescriptionBtn;
   displayTodo(todo);
 };
 
 const addTask = function() {
-  const task = document.getElementById('task_input_box').value;
+  const task = getTask(document);
   fetch('/addTask', {
     method: 'POST',
     body: task
@@ -44,9 +55,9 @@ const getUserTasks = function() {
 };
 
 const createEditTitleBox = function() {
-  const title = document.getElementById('todo_title').textContent;
+  const title = getTodoTitle(document);
   const editTitleBox = `<input type="text" id="new_title" value="${title}"/>
-                        <button id ="add_title" onclick="editTitle()">Add Title</button>`;
+                        <button id ="add_title" onclick="editTitle()">Edit Title</button>`;
   document.getElementById('edit_title').innerHTML = editTitleBox;
 };
 
@@ -58,9 +69,9 @@ const editTitle = function() {
 };
 
 const createEditDescriptionBox = function() {
-  const description = document.getElementById('todo_description').textContent;
+  const description = getTodoDescription(document);
   const editDescriptionBox = `<input type="text" id="new_description" value="${description}"/>
-                        <button id ="add_description" onclick="editDescription()">Add description</button>`;
+                              <button id ="add_description" onclick="editDescription()">Edit Description</button>`;
   document.getElementById('edit_description').innerHTML = editDescriptionBox;
 };
 
