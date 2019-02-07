8e3225a10d4536c3184de225f726baada30bd1f6
mustakim patvekar <44019216+mustakip@users.noreply.github.com>
2019-02-07 12:31:10 +0530

    Add .circleci folder to run ci test on circleci website

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const { createKeyValue, redirectTo } = require('../src/utils');
+const {createKeyValue} = require('../src/utils');
 
 describe('createKeyValue', () => {
   it('should create key value pair of provided input', () => {
@@ -10,24 +10,5 @@ describe('createKeyValue', () => {
     };
     const actualOutput = createKeyValue(content);
     expect(expectedOutput).to.deep.equal(actualOutput);
-  });
-});
-
-describe('redirectTo', () => {
-  it('should set property statuscode and location of response', () => {
-    const res = {
-      headers: {},
-      statusCode: 200,
-      setHeader: (header, value) => (res.headers[header] = value),
-      end: () => {}
-    };
-
-    redirectTo(res, '/home');
-    expect(res)
-      .to.have.property('headers')
-      .to.deep.equal({ Location: '/home' });
-    expect(res)
-      .to.have.property('statusCode')
-      .equals(302);
   });
 });
